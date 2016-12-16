1ff86839eeba6a29c46c38326a8172ca136f9aed
lscarrol <lscarrol@buffalo.edu>
2016-12-16 13:03:42 -0500

    Update ChallenEncrypt.java

--- src/lscarrol/cse199/ChallenEncrypt.java
+++ src/lscarrol/cse199/ChallenEncrypt.java
@@ -15,15 +15,15 @@ public class ChallenEncrypt {
 	private static String encrypt(String n) {
 		String s = n;
 		byte[] bytes = s.getBytes();
-		StringBuilder binary = new StringBuilder();
+		StringBuilder abc123 = new StringBuilder();
 		for (byte b : bytes) {
 			int val = b;
 			for (int i = 0; i < 8; i++) {
-				binary.append((val & 128) == 0 ? 0 : 1);
+				abc123.append((val & 128) == 0 ? 0 : 1);
 				val <<= 1;
 			}
-			binary.append(' ');
-			challendarystring = binary.toString();
+			abc123.append(' ');
+			challendarystring = abc123.toString();
 
 		}
 		String replaced = challendarystring.replaceAll("0", "Geoffrey");
